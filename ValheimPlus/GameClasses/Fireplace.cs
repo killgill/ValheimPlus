1b7b19420e9167f86a804b180aac8aab372d50e4
Grantapher <granttpfr@gmail.com>
2024-10-16 00:31:03 -0700

    Update Fireplace
    * `torches` and `fires` now simply sets `m_infiniteFuel` instead of implementing it itself
    * Made the `Fireplace.UpdateFireplace` transpiler target its insertion point more precisely
    * Fixed bug where fire sources auto-fuel would pull from chests not centered around itself, but possibly around a different fire source
    * 0.219.10 compatibility
    * general cleanup

--- ValheimPlus/GameClasses/Fireplace.cs
+++ ValheimPlus/GameClasses/Fireplace.cs
@@ -1,190 +1,117 @@
-﻿using HarmonyLib;
-using System;
+﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Linq;
 using System.Reflection;
 using System.Reflection.Emit;
+using HarmonyLib;
+using JetBrains.Annotations;
 using ValheimPlus.Configurations;
 using ValheimPlus.Utility;
 
 namespace ValheimPlus.GameClasses
 {
-    class FireplaceFuel
+    internal static class FireplaceFuel
     {
+        /// <summary>
+        /// When fire source is loaded, set its fuel to infinite if necessary.
+        /// </summary>
         [HarmonyPatch(typeof(Fireplace), nameof(Fireplace.Awake))]
         public static class Fireplace_Awake_Patch
         {
-            /// <summary>
-            /// When fire source is loaded in view, check for configurations and set its start fuel and current fuel to max fuel
-            /// </summary>
+            [UsedImplicitly]
             private static void Postfix(ref Fireplace __instance)
             {
-                if (!Configuration.Current.FireSource.IsEnabled || !__instance.m_nview || __instance.m_nview.m_zdo == null) return;
+                var config = Configuration.Current.FireSource;
+                if (!config.IsEnabled) return;
 
-                if (FireplaceExtensions.IsTorch(__instance.m_nview.GetPrefabName()))
-                {
-                    if (Configuration.Current.FireSource.torches)
-                    {
-                        __instance.m_startFuel = __instance.m_maxFuel;
-                        __instance.m_nview.GetZDO().Set("fuel", __instance.m_maxFuel);
-                    }
-                }
-                else if (Configuration.Current.FireSource.fires)
-                {
-                    __instance.m_startFuel = __instance.m_maxFuel;
-                    __instance.m_nview.GetZDO().Set("fuel", __instance.m_maxFuel);
-                }
-            }
-        }
+                var isTorch = IsTorch(__instance.m_nview.GetPrefabName());
+                if ((isTorch && !config.torches) || (!isTorch && !config.fires)) return;
 
-        [HarmonyPatch(typeof(Fireplace), nameof(Fireplace.GetTimeSinceLastUpdate))]
-        public static class Fireplace_GetTimeSinceLastUpdate_Patch
-        {
-            /// <summary>
-            /// If fire source is configured to keep fire source lit, reset time since being lit to 0
-            /// </summary>
-            private static void Postfix(ref double __result, ref Fireplace __instance)
-            {
-                if (!Configuration.Current.FireSource.IsEnabled) return;
-
-                if (FireplaceExtensions.IsTorch(__instance.m_name))
-                {
-                    if (Configuration.Current.FireSource.torches)
-                    {
-                        __result = 0.0;
-                    }
-                }
-                else if (Configuration.Current.FireSource.fires)
-                {
-                    __result = 0.0;
-                }
+                __instance.m_infiniteFuel = true;
             }
         }
 
         [HarmonyPatch(typeof(Fireplace), nameof(Fireplace.UpdateFireplace))]
         public static class Fireplace_UpdateFireplace_Transpiler
         {
-            private static MethodInfo method_ZNetView_IsOwner = AccessTools.Method(typeof(ZNetView), nameof(ZNetView.IsOwner));
-            private static MethodInfo method_addFuelFromNearbyChests = AccessTools.Method(typeof(Fireplace_UpdateFireplace_Transpiler), nameof(Fireplace_UpdateFireplace_Transpiler.AddFuelFromNearbyChests));
+            private static readonly MethodInfo Method_Zdo_SetFloat =
+                AccessTools.Method(typeof(ZDO), nameof(ZDO.GetFloat), new[] { typeof(int), typeof(float) });
+
+            private static readonly MethodInfo Method_AddFuelFromNearbyChests =
+                AccessTools.Method(typeof(Fireplace_UpdateFireplace_Transpiler), nameof(AddFuelFromNearbyChests));
 
             [HarmonyTranspiler]
+            [UsedImplicitly]
             public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
             {
-                if (!Configuration.Current.FireSource.IsEnabled || !Configuration.Current.FireSource.autoFuel) return instructions;
+                var config = Configuration.Current.FireSource;
+                if (!config.IsEnabled || !config.autoFuel) return instructions;
 
-                List<CodeInstruction> il = instructions.ToList();
+                var il = instructions.ToList();
 
                 for (int i = 0; i < il.Count; i++)
                 {
-                    if (il[i].Calls(method_ZNetView_IsOwner))
-                    {
-                        ++i;
-                        il.Insert(++i, new CodeInstruction(OpCodes.Ldarg_0));
-                        il.Insert(++i, new CodeInstruction(OpCodes.Call, method_addFuelFromNearbyChests));
+                    // replace
+                    // ```
+                    // this.m_nview.GetZDO().Set(ZDOVars.s_fuel, num3)
+                    // ```
+                    // with
+                    // ```
+                    // this.m_nview.GetZDO().Set(ZDOVars.s_fuel, AddFuelFromNearbyChests(this) + num3)
+                    // ```
+                    // where num3 is the new amount of fuel to set (normally just the normal decay of fuel)
+                    if (!il[i].Calls(Method_Zdo_SetFloat)) continue;
 
-                        return il.AsEnumerable();
-                    }
+                    i -= 2;
+                    il.Insert(++i, new CodeInstruction(OpCodes.Ldarg_0));
+                    il.Insert(++i, new CodeInstruction(OpCodes.Call, Method_AddFuelFromNearbyChests));
+                    i++; // this is the stloc for num3 
+                    il.Insert(++i, new CodeInstruction(OpCodes.Add));
+
+                    return il;
                 }
 
                 ValheimPlusPlugin.Logger.LogError("Failed to apply Fireplace_UpdateFireplace_Transpiler");
 
-                return instructions;
+                return il;
             }
 
-            private static void AddFuelFromNearbyChests(Fireplace __instance)
+            private static float AddFuelFromNearbyChests(Fireplace __instance)
             {
-                int toMaxFuel = (int)__instance.m_maxFuel - (int)Math.Ceiling(__instance.m_nview.GetZDO().GetFloat("fuel"));
+                var config = Configuration.Current.FireSource;
+                var isTorch = IsTorch(__instance.m_nview.GetPrefabName());
+                // if they are already infinite fuel, don't bother to search for more fuel.
+                if ((isTorch && config.torches) || (!isTorch && config.fires)) return 0;
 
-                if (toMaxFuel > 0)
-                {
-                    Stopwatch delta = GameObjectAssistant.GetStopwatch(__instance.gameObject);
+                int currentFuel = (int)Math.Ceiling(__instance.m_nview.GetZDO().GetFloat(ZDOVars.s_fuel));
+                int toMaxFuel = (int)__instance.m_maxFuel - currentFuel;
+                if (toMaxFuel <= 0) return 0;
 
-                    if (delta.IsRunning && delta.ElapsedMilliseconds < 1000) return;
-                    delta.Restart();
+                var stopwatch = GameObjectAssistant.GetStopwatch(__instance.gameObject);
+                if (stopwatch.IsRunning && stopwatch.ElapsedMilliseconds < 1000) return 0;
 
-                    ItemDrop.ItemData fuelItemData = __instance.m_fuelItem.m_itemData;
+                stopwatch.Restart();
 
-                    int addedFuel = InventoryAssistant.RemoveItemInAmountFromAllNearbyChests(__instance.gameObject, Helper.Clamp(Configuration.Current.FireSource.autoRange, 1, 50), fuelItemData, toMaxFuel, !Configuration.Current.FireSource.ignorePrivateAreaCheck);
-                    if (addedFuel > 0)
-                    {
-                        // Only make the call if we're actually adding fuel, otherwise the fuel adding animation plays every second.
-                        __instance.m_nview.InvokeRPC("RPC_AddFuelAmount", new object[] { (float) addedFuel });
-                        ValheimPlusPlugin.Logger.LogInfo("Added " + addedFuel + " fuel(" + fuelItemData.m_shared.m_name + ") in " + __instance.m_name);
-                    }
-                }
+                var fuelItemData = __instance.m_fuelItem.m_itemData;
+                var range = Helper.Clamp(config.autoRange, 1, 50);
+                int addedFuel = InventoryAssistant.RemoveItemInAmountFromAllNearbyChests(
+                    __instance.gameObject, range, fuelItemData, toMaxFuel, !config.ignorePrivateAreaCheck);
+
+                if (addedFuel <= 0) return 0;
+
+                // Only make the call if we're actually adding fuel,
+                // otherwise the fuel adding animation plays every second.
+                __instance.m_nview.InvokeRPC("RPC_AddFuelAmount", (float)addedFuel);
+                ValheimPlusPlugin.Logger.LogInfo(
+                    $"Added {addedFuel} fuel({fuelItemData.m_shared.m_name}) in {__instance.m_name}");
+
+                return addedFuel;
             }
         }
-    }
 
-    [HarmonyPatch(typeof(Fireplace), nameof(Fireplace.Interact))]
-    public static class Fireplace_Interact_Transpiler
-    {
-        private static List<Container> nearbyChests = null;
+        private static bool IsTorch(string itemName) => TorchItemNames.Contains(itemName);
 
-        private static MethodInfo method_Inventory_HaveItem = AccessTools.Method(typeof(Inventory), nameof(Inventory.HaveItem));
-        private static MethodInfo method_ReplaceInventoryRefByChest = AccessTools.Method(typeof(Fireplace_Interact_Transpiler), nameof(Fireplace_Interact_Transpiler.ReplaceInventoryRefByChest));
-
-        /// <summary>
-        /// Patches out the code that looks for fuel item.
-        /// When no fuel item has been found in the player inventory, check inside nearby chests.
-        /// If found, replace the reference to the player Inventory by the one from the chest.
-        /// </summary>
-        [HarmonyTranspiler]
-        public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
-        {
-            if (!Configuration.Current.CraftFromChest.IsEnabled) return instructions;
-
-            List<CodeInstruction> il = instructions.ToList();
-
-            for (int i = 0; i < il.Count; i++)
-            {
-                if (il[i].Calls(method_Inventory_HaveItem))
-                {
-
-                    // replace call to `inventory.HaveItem(this.m_fuelItem.m_itemData.m_shared.m_name, true)`
-                    // with call to `Fireplace_Interact_Transpiler.ReplaceInventoryRefByChest(ref inventory, this)`.
-                    il[i - 7] = new CodeInstruction(OpCodes.Ldloca_S, 0).MoveLabelsFrom(il[i - 7]);
-                    il[i] = new CodeInstruction(OpCodes.Call, method_ReplaceInventoryRefByChest);
-                    il.RemoveRange(i - 5, 5);
-                    return il.AsEnumerable();
-                }
-            }
-
-            ValheimPlusPlugin.Logger.LogError("Failed to apply Fireplace_Interact_Transpiler");
-
-            return instructions;
-        }
-
-        private static bool ReplaceInventoryRefByChest(ref Inventory inventory, Fireplace fireplace)
-        {
-            string itemName = fireplace.m_fuelItem.m_itemData.m_shared.m_name;
-            if (inventory.HaveItem(itemName)) return true; // original code
-
-            Stopwatch delta = GameObjectAssistant.GetStopwatch(fireplace.gameObject);
-            int lookupInterval = Helper.Clamp(Configuration.Current.CraftFromChest.lookupInterval, 1, 10) * 1000;
-            if (!delta.IsRunning || delta.ElapsedMilliseconds > lookupInterval)
-            {
-                nearbyChests = InventoryAssistant.GetNearbyChests(fireplace.gameObject, Helper.Clamp(Configuration.Current.CraftFromChest.range, 1, 50), !Configuration.Current.CraftFromChest.ignorePrivateAreaCheck);
-                delta.Restart();
-            }
-
-            foreach (Container c in nearbyChests)
-            {
-                if (c.GetInventory().HaveItem(itemName))
-                {
-                    inventory = c.GetInventory();
-                    return true;
-                }
-            }
-
-            return false;
-        }
-    }
-
-    public static class FireplaceExtensions
-    {
-        static readonly string[] torchItemNames = new[]
+        private static readonly HashSet<string> TorchItemNames = new()
         {
             "piece_groundtorch_wood", // standing wood torch
             "piece_groundtorch", // standing iron torch
@@ -196,10 +123,84 @@ namespace ValheimPlus.GameClasses
             "piece_brazierfloor02", // Blue standing brazier
             "piece_jackoturnip" // Jack-o-turnip
         };
+    }
 
-        internal static bool IsTorch(string itemName)
+    [HarmonyPatch(typeof(Fireplace), nameof(Fireplace.Interact))]
+    public static class Fireplace_Interact_Transpiler
+    {
+        // TODO memory leak
+        private static readonly Dictionary<float, List<Container>> NearbyChestsDictionary = new();
+
+        private static readonly MethodInfo Method_Inventory_HaveItem =
+            AccessTools.Method(typeof(Inventory), nameof(Inventory.HaveItem), new[] { typeof(string), typeof(bool) });
+
+        private static readonly MethodInfo Method_ReplaceInventoryRefByChest = AccessTools.Method(
+            typeof(Fireplace_Interact_Transpiler), nameof(ReplaceInventoryRefByChest));
+
+        /// <summary>
+        /// Patches out the code that looks for fuel item.
+        /// When no fuel item has been found in the player inventory, check inside nearby chests.
+        /// If found, replace the reference to the player Inventory by the one from the chest.
+        /// </summary>
+        [HarmonyTranspiler]
+        [UsedImplicitly]
+        public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
         {
-            return torchItemNames.Any(x => x.Equals(itemName));
+            if (!Configuration.Current.CraftFromChest.IsEnabled) return instructions;
+
+            var il = instructions.ToList();
+
+            for (int i = 0; i < il.Count; i++)
+            {
+                if (!il[i].Calls(Method_Inventory_HaveItem)) continue;
+
+                // replace call to `inventory.HaveItem(this.m_fuelItem.m_itemData.m_shared.m_name, true)`
+                // with call to `Fireplace_Interact_Transpiler.ReplaceInventoryRefByChest(ref inventory, this)`.
+                il[i - 7] = new CodeInstruction(OpCodes.Ldloca_S, 1).MoveLabelsFrom(il[i - 7]);
+                il[i] = new CodeInstruction(OpCodes.Call, Method_ReplaceInventoryRefByChest);
+                il.RemoveRange(i - 5, 5);
+                return il.AsEnumerable();
+            }
+
+            ValheimPlusPlugin.Logger.LogError("Failed to apply Fireplace_Interact_Transpiler");
+
+            return il;
+        }
+
+        private static bool ReplaceInventoryRefByChest(ref Inventory inventory, Fireplace fireplace)
+        {
+            string itemName = fireplace.m_fuelItem.m_itemData.m_shared.m_name;
+            if (inventory.HaveItem(itemName)) return true; // original inventory suffices, no need to search further.
+
+            var config = Configuration.Current.CraftFromChest;
+            var fireplaceGameObject = fireplace.gameObject;
+            var stopwatch = GameObjectAssistant.GetStopwatch(fireplaceGameObject);
+            var hash = GameObjectAssistant.GetGameObjectPositionHash(fireplaceGameObject);
+            if (NearbyChestsDictionary.TryGetValue(hash, out var nearbyChests))
+            {
+                int lookupInterval = Helper.Clamp(config.lookupInterval, 1, 10) * 1000;
+                if (!stopwatch.IsRunning || stopwatch.ElapsedMilliseconds > lookupInterval) UpdateNearbyChests();
+            }
+            else
+            {
+                UpdateNearbyChests();
+            }
+
+            var maybeInventory = nearbyChests
+                .Select(container => container.GetInventory())
+                .FirstOrDefault(inv => inv.HaveItem(itemName));
+
+            if (inventory != null) inventory = maybeInventory;
+            return maybeInventory != null;
+
+            void UpdateNearbyChests()
+            {
+                var range = Helper.Clamp(config.range, 1, 50);
+                nearbyChests =
+                    InventoryAssistant.GetNearbyChests(fireplace.gameObject, range, !config.ignorePrivateAreaCheck);
+                stopwatch.Restart();
+                NearbyChestsDictionary[hash] = nearbyChests;
+            }
         }
     }
 }
