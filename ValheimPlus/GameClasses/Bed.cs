6d56588ed010ea350e863dcaf8257238f5bb103c
Grantapher <granttpfr@gmail.com>
2025-05-25 09:27:12 -0700

    #126: Fix not using "Use" hotkey in Bed configs
    Also cleans up the `Bed.cs`.

--- ValheimPlus/GameClasses/Bed.cs
+++ ValheimPlus/GameClasses/Bed.cs
@@ -1,95 +1,70 @@
 ﻿using HarmonyLib;
+using JetBrains.Annotations;
 using UnityEngine;
 using ValheimPlus.Configurations;
 
 namespace ValheimPlus.GameClasses
 {
-	/// <summary>
-	/// Allow to Sleep without setting spawn point
-	/// </summary>
-	[HarmonyPatch(typeof(Bed), "GetHoverText")]
+    public static class BedHelper
+    {
+        public static bool CanSleepWithoutSpawn(this Bed bed) =>
+            Configuration.Current.Bed.IsEnabled &&
+            Configuration.Current.Bed.sleepWithoutSpawn &&
+            !bed.IsCurrent() && // Bed is not already the spawn point.
+            (!Configuration.Current.Bed.unclaimedBedsOnly || bed.GetOwner() == 0L);
 
-	public static class Bed_GetHoverText_Patch
-	{
-		private static void Postfix(Bed __instance, ref string __result, ZNetView ___m_nview)
-		{
-			if (Configuration.Current.Bed.IsEnabled && Configuration.Current.Bed.sleepWithoutSpawn && Configuration.Current.Bed.unclaimedBedsOnly)
-			{
-				bool ownerName = (___m_nview.GetZDO().GetLong("owner", 0L) != 0L) || Traverse.Create(__instance).Method("IsCurrent", new object[0]).GetValue<bool>();
-				if (!ownerName)
-				{
-					__result += Localization.instance.Localize("\n[" + "LShift" + "+<color=yellow><b>$KEY_Use</b></color>] $piece_bed_sleep");
-				}
-			}
-			else if (Configuration.Current.Bed.IsEnabled && Configuration.Current.Bed.sleepWithoutSpawn && !Configuration.Current.Bed.unclaimedBedsOnly)
-			{
-				bool current = Traverse.Create(__instance).Method("IsCurrent", new object[0]).GetValue<bool>();
-				if (current)
-				{
-					return;
-				}
-				__result += Localization.instance.Localize("\n[LShift+<color=yellow><b>$KEY_Use</b></color>] $piece_bed_sleep");
-			}
-		}
-	}
+        // This is pretty much an inline of the Interact code with the ownership checks removed.
+        public static void InteractWithoutOwnershipReadWrite(this Bed bed, Player player)
+        {
+            if (!EnvMan.CanSleep())
+            {
+                player.Message(MessageHud.MessageType.Center, "$msg_cantsleep");
+                return;
+            }
 
-	[HarmonyPatch(typeof(Bed), "Interact")]
-	public static class Bed_Interact_Patch
-	{
-		private static void Postfix(Bed __instance, Humanoid human, bool repeat, ZNetView ___m_nview)
-		{
-			if (Configuration.Current.Bed.IsEnabled && Configuration.Current.Bed.sleepWithoutSpawn)
-			{
-				if (Input.GetKey(KeyCode.LeftShift))
-				{
-					if (Input.GetKeyDown(KeyCode.E))
-					{
-						bool flag = Traverse.Create(__instance).Method("IsCurrent", new object[0]).GetValue<bool>() || repeat || Configuration.Current.Bed.unclaimedBedsOnly && ___m_nview.GetZDO().GetLong("owner", 0L) != 0L;
-						if (!flag)
-						{
-							Player humanPlayer = human as Player;
-							if (!EnvMan.IsAfternoon() && !EnvMan.IsNight())
-							{
-								human.Message(MessageHud.MessageType.Center, "$msg_cantsleep", 0, null);
-								return;
-							}
-							if (!__instance.CheckEnemies(humanPlayer))
-							{
-								return;
-							}
-							if (!__instance.CheckExposure(humanPlayer))
-							{
-								return;
-							}
-							if (!__instance.CheckFire(humanPlayer))
-							{
-								return;
-							}
-							if (!__instance.CheckWet(humanPlayer))
-							{
-								return;
-							}
-							human.AttachStart(__instance.m_spawnPoint, __instance.gameObject, true, true, false, "attach_bed", new Vector3(0f, 0.5f, 0f));
-							return;
-						}
-					}
-				}
-			}
-		}
-	}
-	[HarmonyPatch(typeof(Bed), "Interact")]
-	public static class Bed_InteractIgnoreStandaloneE_Patch
-	{
-		private static bool Prefix(Bed __instance, Humanoid human)
-		{
-			if (Input.GetKey(KeyCode.E))
-			{
-				if (Input.GetKey(KeyCode.LeftShift))
-				{
-					return false;
-				}
-			}
-			return true;
-		}
-	}
+            if (!bed.CheckEnemies(player) ||
+                !bed.CheckExposure(player) ||
+                !bed.CheckFire(player) ||
+                !bed.CheckWet(player)) return;
+
+            player.AttachStart(bed.m_spawnPoint, bed.gameObject, true, true, false,
+                "attach_bed", new Vector3(0.0f, 0.5f, 0.0f));
+        }
+    }
+
+    [HarmonyPatch(typeof(Bed), nameof(Bed.GetHoverText))]
+    public static class Bed_GetHoverText_Patch
+    {
+        private const string AppendStr = "\n[LShift+<color=yellow><b>$KEY_Use</b></color>] $piece_bed_sleep";
+
+        [UsedImplicitly]
+        private static void Postfix(Bed __instance, ref string __result)
+        {
+            if (!__instance.CanSleepWithoutSpawn()) return;
+            __result += Localization.instance.Localize(AppendStr);
+        }
+    }
+
+
+    [HarmonyPatch(typeof(Bed), nameof(Bed.Interact))]
+    public static class Bed_Interact_Patch
+    {
+        // Avoid `shift + use` triggering the `use` path.
+        [UsedImplicitly]
+        private static bool Prefix(Bed __instance, Humanoid human)
+        {
+            return !ZInput.GetButtonDown("Use") || !ZInput.GetKey(KeyCode.LeftShift);
+        }
+
+        [UsedImplicitly]
+        private static void Postfix(Bed __instance, Humanoid human, bool repeat)
+        {
+            if (repeat ||
+                !__instance.CanSleepWithoutSpawn() ||
+                !ZInput.GetKey(KeyCode.LeftShift) ||
+                !ZInput.GetButtonDown("Use")) return;
+
+            __instance.InteractWithoutOwnershipReadWrite(human as Player);
+        }
+    }
 }
